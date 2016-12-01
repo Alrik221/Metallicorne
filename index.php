1bf36eb04f66d43583fb8e14fcdf5c660f948d95
audrena <ayme.audren@gmail.com>
2016-12-02 00:04:53 +0100

    Header : menu + css

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+
+<html>
+	<head>
+		<title>METALLICORNE</title>
+		<meta charset='UTF-8'>
+		<link rel="stylesheet" type="text/css" href="public/simplegrid.css" />
+		<link rel="stylesheet" type="text/css" href="public/reset.css" />
+	</head>
+	<body>
+		<?php include "views/header.php" ?>
+	</body>
+</html> 
