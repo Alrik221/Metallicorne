c43ca6d9dbca823fd362f2a13d147353843b7e80
audrena <ayme.audren@gmail.com>
2016-12-02 01:30:17 +0100

    commit index.php clean

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 
 <html>
 	<head>
+<<<<<<< HEAD
 		<meta charset='UTF-8'>
 	</head>
 	<body>			
 		<img src="image/licorne.jpg" alt="licornemetal">
+=======
+		<title>METALLICORNE</title>
+		<meta charset='UTF-8'>
+		<link rel="stylesheet" type="text/css" href="public/simplegrid.css" />
+		<link rel="stylesheet" type="text/css" href="public/reset.css" />
+	</head>
+	<body>
+		<?php include "views/header.php" ?>
+>>>>>>> 94031878934695d45a7bd1241790f8df0f5d09ee
 	</body>
 </html> 
