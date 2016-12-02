cdf1d127be56aaaf66a39339ec2f0fd0c36f8db9
REMOND Alexis <alexis.remond@etud.u-picardie.fr>
2016-12-02 07:00:17 +0100

    Ajout du footer !

---
+++ views/footer.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" type="text/css" href="public/style_footer.css" />
+<link rel="stylesheet" type="text/css" href="public/simplegrid.css" />
+<footer id="footer">
+        <div class="grid grid-pad">
+			<div class="col-9-12">
+				<h4>Metallicornes</h4>
+					<div class="col-1-4">
+						<ul>
+							<li>Alexis Rémond</li>
+							<li>Cyprien Pecquet</li>
+							<li>Louise Crampon</li>
+							<li>Elisa Pluyette</li>
+						</ul>
+					</div>
+					<div class="col-1-4">
+						<ul>
+							<li>Dorian Mestdagh</li>
+							<li>Thomas Bévière</li>
+							<li>Antoine Payot</li>
+							<li>Audren Aymé</li>
+						</ul>
+					</div>
+				</div>
+            <div class="col-3-12">
+                <h4>Information</h4>
+					<p class="text-right">Copyright© METALLICORNE 2016.<br/>All right reserved.</p>
+            </div>
+        </div>       
+</footer>
