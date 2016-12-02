95a43481bacb98fe7ca5fe753c2f3722b280ff3d
audrena <ayme.audren@gmail.com>
2016-12-02 02:08:53 +0100

    Mise en page des services (boot)

--- index.php
+++ index.php
@@ -2,12 +2,6 @@
 
 <html>
 	<head>
-<<<<<<< HEAD
-		<meta charset='UTF-8'>
-	</head>
-	<body>			
-		<img src="image/licorne.jpg" alt="licornemetal">
-=======
 		<title>METALLICORNE</title>
 		<meta charset='UTF-8'>
 		<link rel="stylesheet" type="text/css" href="public/simplegrid.css" />
@@ -15,6 +9,5 @@
 	</head>
 	<body>
 		<?php include "views/header.php" ?>
->>>>>>> 94031878934695d45a7bd1241790f8df0f5d09ee
 	</body>
 </html> 
---
+++ views/service.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../public/style_service.css" rel="stylesheet">
+<body>
+	<form method="post" action="ajout.php">
+		<div class="form-group">
+	    	<label for="exampleInputEmail1">Nom du camp</label>
+	    	<input type="email" class="form-control" id="exampleInputName" placeholder="Name">
+	  </div>
+	  <div class="form-group">
+	    	<label for="exampleInputPassword1">Adresse du camp</label>
+	    	<input type="password" class="form-control" id="exampleInputAdresse" placeholder="Adresse">
+	  </div>
+	  <div class="form-group">
+	    	<label for="exampleInputFile">Ville du camp</label>
+	    	<input type="password" class="form-control" id="exampleInputAdresse" placeholder="Adresse">
+	    <!--<p class="help-block">Example block-level help text here.</p>!-->
+	  </div>
+	  <button type="submit" class="btn btn-danger">Submit</button>
+	</form>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
